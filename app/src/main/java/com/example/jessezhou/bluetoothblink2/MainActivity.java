630849d1f1a9bdeb8cb9cb147b77e1cad50ec7a8
Jesse Zhou <jesse.cy.zhou@gmail.com>
2014-12-25 16:26:26 -0800

    Able to turn on/off bt,, get device list
    Able to the most basic functions of bluetooth at the moment. Is allowed
    to turn on discovery as well

--- app/src/main/java/com/example/jessezhou/bluetoothblink2/MainActivity.java
+++ app/src/main/java/com/example/jessezhou/bluetoothblink2/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.jessezhou.bluetoothblink2;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 
@@ -21,7 +26,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        init();
 
     }
 
@@ -31,13 +36,47 @@ public class MainActivity extends ActionBarActivity {
         bVisible = (Button)findViewById(R.id.bVisible);
         bList = (Button)findViewById(R.id.bList);
 
-        ListView listView = (ListView)findViewById(R.id.listView);
+        listView = (ListView)findViewById(R.id.listView);
 
         btAdapter = BluetoothAdapter.getDefaultAdapter();
     }
 
-    public void on(){
-        
+    public void on(View view){
+        if(!btAdapter.isEnabled()){
+            Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnOn, 0);
+            Toast.makeText(getApplicationContext(), "Turned on"
+                    , Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Already on", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void visible(View view){
+        Intent getVisible = new Intent(BluetoothAdapter.
+                ACTION_REQUEST_DISCOVERABLE);
+        startActivityForResult(getVisible, 0);
+
+    }
+
+    public void list(View view){
+        pairedDevices = btAdapter.getBondedDevices();
+
+        ArrayList list = new ArrayList();
+        for(BluetoothDevice bt : pairedDevices)
+            list.add(bt.getName() + "\n" + bt.getAddress());
+
+        Toast.makeText(getApplicationContext(),"Showing Paired Devices", Toast.LENGTH_SHORT).show();
+        final ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+    }
+
+    public void off(View view){
+        btAdapter.disable();
+        Toast.makeText(getApplicationContext(), "Bluetooth Off", Toast.LENGTH_LONG).show();
+
     }
 
 }
